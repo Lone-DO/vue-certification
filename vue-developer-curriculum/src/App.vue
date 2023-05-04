c5ee645f3c84d2e6c7f9bc3ece1f7838a991a842
lone-do <lotussythe@gmail.com>
2023-05-04 07:26:57 -0400

    eslint

--- vue-developer-curriculum/src/App.vue
+++ vue-developer-curriculum/src/App.vue
@@ -1,24 +1,26 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/icons/logo.svg" width="125" height="125" />
+  <div id="app">
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/icons/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/reactivity">Reactivity</RouterLink>
-      </nav>
-    </div>
-  </header>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/reactivity">Reactivity</RouterLink>
+        </nav>
+      </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
--- vue-developer-curriculum/vite.config.js
+++ vue-developer-curriculum/vite.config.js
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   css: {
     preprocessorOptions: {
@@ -18,8 +18,8 @@ export default defineConfig({
           @import "./src/assets/styles/_mixins.scss";
           @import "./src/assets/styles/_colors.scss";
           @import "./src/assets/styles/_variables.scss";
-        `
-      }
-    }
-  }
-})
+        `,
+      },
+    },
+  },
+});
