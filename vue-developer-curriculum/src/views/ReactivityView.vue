6cf2b8a6ba13b41417473bad2b682ef328dab4c9
lone-do <lotussythe@gmail.com>
2023-05-04 05:42:31 -0400

    first commit

---
+++ vue-developer-curriculum/src/views/ReactivityView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id='reactive'>
+    <span>Current Count: {{ state.count }}</span>
+    <button @click='increment'>+</button>
+    <button @click='decrement'>-</button>
+  </div>
+</template>
+
+<script setup lang='ts'>
+  import { reactive } from 'vue';
+  const state = reactive({ count: 0});
+  function increment() {
+    state.count++;
+  }  
+  function decrement() {
+    state.count--;
+  }
+</script>
+
+<style lang='scss'>
+  #reactive {
+    gap: .5rem;
+    display: flex;
+  }
+</style>
