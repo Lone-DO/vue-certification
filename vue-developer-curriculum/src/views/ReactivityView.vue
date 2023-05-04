1fc6424a530409834b6cc0e6e57ea8959ee5ea85
lone-do <lotussythe@gmail.com>
2023-05-04 06:40:15 -0400

    Reactive Objects/Arrays

--- vue-developer-curriculum/src/views/ReactivityView.vue
+++ vue-developer-curriculum/src/views/ReactivityView.vue
@@ -5,23 +5,53 @@
       <button @click='increment'>+</button>
       <button @click='decrement'>-</button>
     </div>
+    <form id='reactive-form' @submit.prevent='add' >
+      <label for='name'>Name</label>
+      <input name='name' v-model='name'/>
+      <button type='submit'>Add Person</button>
+    </form>
+    <fieldset id='reactive-list'>
+      <legend>List of People</legend>
+      <ul>
+        <li v-for='person in state.people' :key='person'>{{ person }}</li>
+      </ul>
+    </fieldset>
   </div>
 </template>
 
 <script setup lang='ts'>
-  import { reactive } from 'vue';
-  const state = reactive({ count: 0});
+  import { ref, reactive } from 'vue';
+  import { Ref } from 'vue';
+  /** Declared Types */
+
+  interface State { 
+    count: number, 
+    people: Array<string>, 
+  };
+
+  /** State */
+  const state: State = reactive({ count: 0, people: [] });
+  const name: Ref<string> = ref('');
+  // const name = ref<string>('');
+
+  /** Methods */
   function increment() {
     state.count++;
   }  
   function decrement() {
     state.count--;
   }
+  function add() {
+    if (!state.people.includes(name.value)) {
+      state.people.push(name.value);
+    }
+    name.value = '';
+  }
 </script>
 
 <style lang='scss'>
   #reactive {
-    &-container {
+    &-container, &-form {
       gap: .5rem;
       display: flex;
     }
