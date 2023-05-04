788f12f90f42eeb090c27950bc1c64af078ca036
lone-do <lotussythe@gmail.com>
2023-05-04 07:51:10 -0400

    Add event composable

--- src/composables/mouse.ts
+++ src/composables/mouse.ts
@@ -1,16 +1,14 @@
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref } from 'vue';
+import { useEventListener } from './event';
 
 export function useMouse() {
   const x = ref(0);
   const y = ref(0);
 
-  function update(event: MouseEvent) {
+  useEventListener(window, 'mousemove', (event: MouseEvent) => {
     x.value = event.pageX;
     y.value = event.pageY;
-  }
-
-  onMounted(() => window.addEventListener('mousemove', update));
-  onUnmounted(() => window.removeEventListener('mousemove', update));
+  });
 
   return { x, y };
 }
