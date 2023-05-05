893e1023a5f75d51c2cb9b5fdcb5c66ce88b0386
lone-do <lotussythe@gmail.com>
2023-05-05 11:24:30 -0400

    Movie Rating - Finish Base component

---
+++ Code Challenges/create-a-movie-rating-app-1/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { items as movies } from './movies.json';
+import MovieCard from './MovieCard.vue';
+/*
+ This is an Icon that you can use to represent the stars if you like
+ otherwise you could just use a simple ⭐️ emoji, or * character.
+*/
+// import { StarIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <!-- This is where your template goes	-->
+  <main id="root">
+    <section id="content">
+      <MovieCard v-for="movie in movies" :key="movie.id" v-bind="{ movie }" />
+    </section>
+  </main>
+</template>
+
+<style lang="scss">
+#root {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #content {
+    gap: 1rem;
+    padding: 7rem 3rem;
+    display: flex;
+    align-items: stretch;
+    width: 100%;
+    flex-wrap: wrap;
+    overflow: auto;
+    max-height: 100%;
+    justify-content: center;
+  }
+}
+</style>
