3cd735bd35da147c9f64e8cad0d1018dd6a5e252
Lone-DO <lotussythe@gmail.com>
2024-03-02 21:56:22 +0100

    Lifecycle state movies state management (local)

--- Code Challenges/create-a-movie-rating-app-1/src/App.vue
+++ Code Challenges/create-a-movie-rating-app-1/src/App.vue
@@ -1,15 +1,77 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import api from './common/api.js';
 import { items as _movies } from './movies.json';
 import MovieCard from './MovieCard.vue';
 
-const parsed = _movies.map((movie) => ({ ...movie, ratings: [1, 2, 3, 4, 5] }));
-const movies = ref(parsed);
+const genres = ref([]);
+const movies = ref([]);
+const storedMovies = ref([]);
+
+const RATINGS = [1, 2, 3, 4, 5];
+
+class Movie {
+  constructor(data) {
+    this.raw = data;
+    this.name = String(data?.name || '');
+    this.description = String(data?.description || '');
+    this.image = String(data?.image || '');
+    this.genre = String(data?.genre || '');
+    this.released = Boolean(data?.released || false);
+    this.rating = Number(data?.rating || 0);
+    this.ratings = RATINGS;
+
+    // return this.unpack(data);
+  }
+
+  // unpack(data) {
+  //   if (data) {
+  //     for (const key in data) {
+  //       if (this[key] === undefined) this[key] = data[key];
+  //     }
+  //   }
+  //   return this;
+  // }
+}
+
+async function getGenres() {
+  try {
+    genres.value = await api.get('/title/list-popular-genres');
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function updateRating(movie = {}, rating = 0) {
   movie.rating = rating;
+  return saveMovies();
 }
 
+function restoreMovies() {
+  storedMovies.value = JSON.parse(localStorage.getItem('movies') || '[]');
+  if (!storedMovies.value || !storedMovies.value?.length) storedMovies.value = _movies;
+  return Promise.resolve(storedMovies);
+}
+
+function saveMovies() {
+  if (!movies.value) return Promise.resolve();
+  const data = JSON.stringify(movies.value);
+  return data ? localStorage.setItem('movies', data) : null;
+}
+
+function addMovie(movie) {
+  return movie && movies.value.push(new Movie(movie));
+}
+
+function parseMovies(data) {
+  return data && data.map((movie) => new Movie(movie));
+}
+
+onMounted(async () => {
+  await restoreMovies();
+  // await getGenres();
+  movies.value = await parseMovies(storedMovies.value);
+});
 /*
  This is an Icon that you can use to represent the stars if you like
  otherwise you could just use a simple ⭐️ emoji, or * character.
@@ -21,7 +83,7 @@ function updateRating(movie = {}, rating = 0) {
   <!-- This is where your template goes	-->
   <main id="root">
     <section id="content">
-      <MovieCard v-for="movie in movies" :key="movie.id" v-bind="{ movie, updateRating }" />
+      <MovieCard v-for="movie in movies" :key="movie.id" v-bind="{ movie, updateRating, genres }" />
     </section>
   </main>
 </template>
