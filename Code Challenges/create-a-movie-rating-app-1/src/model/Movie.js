7d592de19769dcf254379f86c0cc0128deebbb5c
Lone-DO <lotussythe@gmail.com>
2024-03-07 00:55:33 +0100

    Export Movie class to independent file

---
+++ Code Challenges/create-a-movie-rating-app-1/src/model/Movie.js
@@ -0,0 +1,25 @@
+const RATINGS = [1, 2, 3, 4, 5];
+
+export default class Movie {
+  constructor(data) {
+    this.raw = data;
+    this.name = String(data?.name || '');
+    this.description = String(data?.description || '');
+    this.image = String(data?.image || '');
+    this.genre = String(data?.genre || '');
+    this.released = Boolean(data?.released || false);
+    this.rating = Number(data?.rating || 0);
+    this.ratings = RATINGS;
+
+    // return this.unpack(data);
+  }
+
+  // unpack(data) {
+  //   if (data) {
+  //     for (const key in data) {
+  //       if (this[key] === undefined) this[key] = data[key];
+  //     }
+  //   }
+  //   return this;
+  // }
+}
